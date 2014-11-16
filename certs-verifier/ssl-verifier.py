efd02f5d800a9ae2955229faff45ee455197a57c
Antonio Cesar Vargas <vargasces@gmail.com>
2014-11-16 09:42:02 -0500

    Fix errors and can connect now to different sites to verify their SSL cert

--- certs-verifier/ssl-verifier.py
+++ certs-verifier/ssl-verifier.py
@@ -3,17 +3,40 @@ from M2Crypto import SSL
 import re
 
 
-def connect():
-    serv_addr = ('107.21.250.35', 443)
-    ctx = SSL.Context()
-    s = SSL.Connection(ctx)
-    s.connect(serv_addr)
-    mycert = s.get_peer_cert()
-    plain_text = re.sub('\n', '', mycert.as_text())
-    p = re.compile(r"Signature Algorithm: \w+ ")
-    match = p.search(plain_text)
-    print match.group()
-    # subject = mycert.get_subject()
+class ShaChecker(object):
+    def __init__(self, site):
+        self.site = site
+        self.result(self.site)
+
+    def _connect(self, site):
+        serv_addr = (site, 443)
+        ctx = SSL.Context()
+        s = SSL.Connection(ctx)
+        s.connect(serv_addr)
+        mycert = s.get_peer_cert()
+        plain_text = re.sub('\n', '', mycert.as_text())
+        p = re.compile(r"Signature Algorithm: \w+ ")
+        match = p.search(plain_text)
+        return match.group()
+
+    def _get_algorithm(self, site):
+        algorithm = self._connect(site)
+        return algorithm
+
+    def is_sha256_present(self, site):
+        alg = self._get_algorithm(site)
+        _, _, sha = alg.split()
+        answer = False
+        if sha == 'sha256WithRSAEncryption':
+            answer = True
+        return answer
+
+    def result(self, site):
+        if self.is_sha256_present(site):
+            print "{} non-vulnerable".format(site)
+        else:
+            print "{} vulnerable".format(site)
+
 
 class ProcessFile(object):
     def __init__(self, filename):
@@ -45,4 +68,4 @@ if __name__ == "__main__":
     process_file = ProcessFile('domains.txt')
     domains = process_file.get_domains()
     for entry in domains:
-        print entry
+        ShaChecker(entry)
